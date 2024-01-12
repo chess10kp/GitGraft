4a5e0aee3926d0189cf30ac71babea73fe150c82
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-01-12 02:00:54 -0500

    feat: Expense Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 import Form from './components/Form'
 import Expense from './components/Expense'
 
-type Expense = {
+type Props = {
   id: string // to use it as a key
   description: string
   amount: Number
@@ -31,7 +31,8 @@ function App() {
 
   const onLoginHandler = async () => {
     try {
-      const response = await fetch(`http://localhost:8000/login/${username.current.value}/${password.current.value}`)
+      // @ts-ignore
+      const response = await fetch(`http://localhost:8000/login/${username.current.value}/${password.current.value}`) 
       const data = await response.json()
       console.log(typeof(data))
     } catch (error) {
@@ -46,9 +47,8 @@ function App() {
 
   return (
     <div className='app'>
-      <Form username={username} password={password} clickHandler={onLoginHandler} ></Form>
-      {spenders.map((spender: Expense) => (
-        <Expense key={spender.id} amount={spender.amount} expense={spender.description}></Expense>
+      {spenders.map((spender: Props) => (
+        <Expense key={spender.id} timestamp={spender.timestamp} amount={spender.amount} description={spender.description}></Expense>
       ))}
     </div>
   )
