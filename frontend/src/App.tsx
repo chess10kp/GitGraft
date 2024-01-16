647ab1d2e46dd60b21cf57a9cf57c47cc70e9bc7
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-01-16 00:14:42 -0500

    fix: updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,28 @@
 import { useState, useEffect, useRef } from 'react'
+import {useDisclosure} from '@chakra-ui/react'
 
 import './App.css'
 
 import Form from './components/Form'
 import Expense from './components/Expense'
+import ExpenseForm from './components/ExpenseForm'
+import RegisterForm from './components/RegisterForm'
 
 type Props = {
   id: string // to use it as a key
   description: string
   amount: Number
   timestamp: string
+  category: string
 }
 
 function App() {
   const username = useRef("")
   const password = useRef("")
   const [spenders, setSpenders] = useState([])
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const isLoggedIn = useRef(false)
+  
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
 
   const find_spenders = async () => {
@@ -47,8 +53,13 @@ function App() {
 
   return (
     <div className='app'>
+      <RegisterForm isOpen={isOpen} onClose={onClose}></RegisterForm>
+      <button onClick={onOpen}>Open login</button> 
+      <Form username={username} password={password} clickHandler={onLoginHandler}></Form>
+      <ExpenseForm isLoggedIn={isLoggedIn.current}></ExpenseForm>
       {spenders.map((spender: Props) => (
-        <Expense key={spender.id} timestamp={spender.timestamp} amount={spender.amount} description={spender.description}></Expense>
+        // @ts-ignore
+        <Expense key={spender.id} category={spender.category} timestamp={spender.timestamp} amount={spender.amount} description={spender.description}></Expense>
       ))}
     </div>
   )
