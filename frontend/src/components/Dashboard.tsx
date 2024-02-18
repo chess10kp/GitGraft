d0779e9c6648fa935dc5b787f508aa76647220b3
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-02-17 23:54:55 -0500

    feat: working pie chart

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -5,37 +5,49 @@ import {
   StatNumber,
   StatHelpText,
   StatArrow,
+  Box
 } from '@chakra-ui/react'
 
 
+
 type Props = {  
-  expenses : [] 
+  expenses : [{amount: string, category: string, description: string}]
 }
+
+import PieChart from "./PieChart"
 
 
 const Dashboard = (props: Props) => {
-  console.log(props.expenses)
   const t_expenses = props.expenses.filter((expense: any) => 
     parseInt(expense.amount) > 0 
-  ).reduce((acc: number, expenses: any) => acc + parseInt(expenses.amount) , 0) 
+  )
+  const t_expense = t_expenses.reduce((acc: number, expenses: any) => acc + parseInt(expenses.amount) , 0) 
+
   const t_savings = props.expenses.filter((expense: any) => 
     parseInt(expense.amount) < 0 
-  ).reduce((acc: number, expenses: any) => acc + parseInt(expenses.amount) , 0) 
+  )
+  const t_saving = t_savings.reduce((acc: number, expenses: any) => acc + parseInt(expenses.amount) , 0) 
+
+  const net = t_saving - t_expense
+
  return (
+    <Box>
     <Flex>
       <Stat>
-        <StatLabel>Balance</StatLabel>
-        <StatNumber>${t_expenses}</StatNumber>
-        <StatHelpText>Total expenses <StatArrow/>
+        <StatLabel>Expenses</StatLabel>
+        <StatNumber>-${t_expense}</StatNumber>
+        <StatHelpText>{net < 0 ? `Expenes uncleared ${-net*100/t_expense}%` : null}<StatArrow/>
         </StatHelpText>
+      <PieChart transactions={t_expenses}></PieChart>
       </Stat>
+
       <Stat>
         <StatLabel>Savings</StatLabel>
-        <StatNumber>${t_savings}</StatNumber>
-        <StatHelpText>Total expenses <StatArrow/>
-        </StatHelpText>
+        <StatNumber>${t_saving}</StatNumber>
+        <StatHelpText>{net > 0 ? `${t_saving * 100 / t_expense} percent higher than expenses` : null }</StatHelpText>
       </Stat>
     </Flex>
+  </Box>
   )
 }
 
